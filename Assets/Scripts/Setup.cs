3300ba6194eb7f986948114099ab7d71f2f35d49
watsonjustina1 <watsonjustina1@gmail.com>
2026-09-09 14:57:01 -0300

    Script paredes coliseo

--- Assets/Scripts/Setup.cs
+++ Assets/Scripts/Setup.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class Setup : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject bloque;
+    public int cantidad = 60;
+    public float radio = 5f;
+    public float altura = 0f;
+
     void Start()
     {
-        
+        CrearPared();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CrearPared()
     {
-        
+        if (bloque == null)
+        {
+            Debug.LogWarning("Por favor, asigna un prefab en la variable 'bloque'.");
+            return;
+        }
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            float angulo = i * (360f / cantidad);
+            float rad = angulo * Mathf.Deg2Rad;
+
+            float x = transform.position.x + Mathf.Cos(rad) * radio;
+            float z = transform.position.z + Mathf.Sin(rad) * radio;
+
+            Vector3 posicion = new Vector3(x, altura, z);
+
+            GameObject nuevoBloque = (GameObject)Instantiate(bloque, posicion, Quaternion.identity);
+            nuevoBloque.transform.LookAt(new Vector3(transform.position.x, nuevoBloque.transform.position.y, transform.position.z));
+        }
     }
 }
