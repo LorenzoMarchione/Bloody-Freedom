e332e5bcfc4ce20fdd0a4a9ab9270e7465ac9ea5
LorenzoMarchione <lorenmarchione@gmail.com>
2026-09-10 12:37:01 -0300

    feat: hit and dead state base logic

--- Assets/Scripts/Enemy/States/EnemyDeadState.cs
+++ Assets/Scripts/Enemy/States/EnemyDeadState.cs
@@ -3,4 +3,11 @@ using UnityEngine;
 public class EnemyDeadState : EnemyState
 {
     public EnemyDeadState(Enemy enem) : base(enem) { }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        enemy.StopMovement();
+    }
 }
--- Assets/Scripts/Enemy/States/EnemyHitState.cs
+++ Assets/Scripts/Enemy/States/EnemyHitState.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class EnemyHitState : EnemyState
 {
+    private float recoilTimer;
     public EnemyHitState(Enemy enem) : base (enem) { }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        recoilTimer = config.RecoilDuration;
+        enemy.StepIn(config.StepInSpeed, -enemy.TargetDirection);
+        enemy.BasicAttack();
+    }
+    public override void Update() => recoilTimer -= Time.deltaTime;
+    public override void FixedUpdate()
+    {
+        if (recoilTimer <= 0)
+        {
+            enemy.StopMovement();
+            stateMachine.ChangeState(enemy.ChaseState);
+        }
+    }
 }
